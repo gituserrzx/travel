a1039db5a30ff297bf5e5491fe79083d8804ec0d
zx <202964651@qq.com>
2019-03-02 12:30:24 +0800

    home-header

---
+++ src/components/home/homeHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss" scoped>
+  @import 'assets/varibles.scss';
+  .header{
+    display: flex;
+    line-height: .86rem;
+    color: #fff;
+    background-color: $bgColor;
+    .header-left{
+      width: .64rem;
+      float: left;
+      text-align: center;
+      font-size: .20rem
+    }
+    .header-input{
+      flex: 1;
+      height: .64rem;
+      line-height: .64rem;
+      margin-left: .2rem;
+      margin-top: .12rem;
+      background-color: #fff;
+      border-radius: .1rem;
+      color: #cacaca;
+      padding-left: .2rem;
+    }
+    .header-right {
+      width: 1.24rem;
+      float: right;
+      text-align: center;
+    }
+      .arrow-icon{
+        font-size: .24rem;
+        margin-left: -0.06rem
+    }
+  }
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from 'components/home/homeHeader'
+  export default {
+    components: {
+      HomeHeader
+    }
+  }
+</script>
+
+<style  lang="scss">
+</style>
+
