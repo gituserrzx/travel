288a5a62257c201da1c3039e04d16518fc682694
zx <202964651@qq.com>
2019-03-02 19:23:04 +0800

    swiper

---
+++ src/components/home/swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption" >
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img  class="swiper-img" :src='item.imgUrl' >
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+
+    export default {
+      name:'homeSwiper',
+      data() {
+        return {
+          swiperOption: {
+            pagination: '.swiper-pagination',
+            loop: true
+          },
+          swiperList: [{
+            id: '0001',
+            imgUrl: 'http://img4.imgtn.bdimg.com/it/u=338830099,220214673&fm=26&gp=0.jpg'
+          },{
+            id: '0002',
+            imgUrl: 'http://img3.imgtn.bdimg.com/it/u=366334772,3752520079&fm=26&gp=0.jpg'
+          }]
+        }
+      }
+    }
+</script>
+
+<style lang="scss">
+  .wrapper >>> .swiper-pagination-bullet-active{
+    background-color: white!important;
+  }
+  .wrapper{
+    overflow: hidden;
+  height: 0;
+  width: 100%;
+  padding-bottom: 36%;
+  background-color: #eee;
+  
+  .swiper-img{
+    width: 100%;
+    height: 100%;
+  }
+  }
+ 
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <home-header></home-header>
+    <swiper></swiper>
   </div>
 </template>
 
 <script>
   import HomeHeader from 'components/home/homeHeader'
+  import Swiper from 'components/home/swiper'
   export default {
     components: {
-      HomeHeader
+      HomeHeader,
+      Swiper
     }
   }
 </script>
